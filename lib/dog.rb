385e55a727925ae4fb096f79afd980074e48369f
Madison <madivallarino@gmail.com>
2021-09-21 17:00:09 -0400

    commit tree 054527b65c321fb8d7dc4de1cb9436d3524350fb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-
+    # setting
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+    end
+    # getting
+    def name
+        @this_dogs_name
+    end
 end
+
+
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
+
+binding.pry
